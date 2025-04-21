92fd392ebab271ec6266f0d8093e21770f7d3d10
hamintonjair <hamintonjair@gmail.com>
2025-04-21 07:05:00 -0500

    otro cambio en database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,36 +24,36 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public array $defaultt;
+    // public array $default;
 
     public function __construct()
     {
         parent::__construct();
-        $this->default = [
-            'DSN'          => '',
-            'hostname'     => 'localhost',
-            'username'     => 'root',
-            'password'     => '',
-            'database'     => 'db_span',
-            'DBDriver'     => 'MySQLi',
-            'DBPrefix'     => '',
-            'pConnect'     => false,
-            'DBDebug'      => true,
-            'charset'      => 'utf8mb4',
-            'DBCollat'     => 'utf8mb4_general_ci',
-            'swapPre'      => '',
-            'encrypt'      => false,
-            'compress'     => false,
-            'strictOn'     => false,
-            'failover'     => [],
-            'port'         => 3306,
-            'numberNative' => false,
-            'dateFormat'   => [
-                'date'     => 'Y-m-d',
-                'datetime' => 'Y-m-d H:i:s',
-                'time'     => 'H:i:s',
-            ],
-        ];
+        // $this->default = [
+        //     'DSN'          => '',
+        //     'hostname'     => 'localhost',
+        //     'username'     => 'root',
+        //     'password'     => '',
+        //     'database'     => 'db_span',
+        //     'DBDriver'     => 'MySQLi',
+        //     'DBPrefix'     => '',
+        //     'pConnect'     => false,
+        //     'DBDebug'      => true,
+        //     'charset'      => 'utf8mb4',
+        //     'DBCollat'     => 'utf8mb4_general_ci',
+        //     'swapPre'      => '',
+        //     'encrypt'      => false,
+        //     'compress'     => false,
+        //     'strictOn'     => false,
+        //     'failover'     => [],
+        //     'port'         => 3306,
+        //     'numberNative' => false,
+        //     'dateFormat'   => [
+        //         'date'     => 'Y-m-d',
+        //         'datetime' => 'Y-m-d H:i:s',
+        //         'time'     => 'H:i:s',
+        //     ],
+        // ];
 
         // Lógica para entorno de pruebas
         if (ENVIRONMENT === 'testing') {
